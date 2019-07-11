aef8269862422ce88126a3104c00d51c9c4fb397
Ildar Mulyukov <ildar@altlinux.ru>
2019-07-11 11:49:07 +0000

    tango/server/nodemcu/net.lua: implement onreceive

--- spec/tango_server_nodemcu_net_spec.lua
+++ spec/tango_server_nodemcu_net_spec.lua
@@ -9,5 +9,29 @@ describe("tango.server.nodemcu.net module", function ()
         
         tsnn_module.onreceive(conn, testchunk)
         assert.stub(dispatcher.dispatch).was.called_with(match._, "0123456789")
+        dispatcher.dispatch:revert()
       end)
+    it("should be able to receive chucked data", function ()
+        local testchunk1, testchunk2 = "10\n012", "3456789"
+        stub(dispatcher, "dispatch")
+        
+        tsnn_module.onreceive(conn, testchunk1)
+        assert.stub(dispatcher.dispatch).was_not.called()
+        tsnn_module.onreceive(conn, testchunk2)
+        assert.stub(dispatcher.dispatch).was.called_with(match._, "0123456789")
+        dispatcher.dispatch:revert()
+        
+        testchunk1, testchunk2 = "1", "0\n0123456789"
+        stub(dispatcher, "dispatch")
+        
+        tsnn_module.onreceive(conn, testchunk1)
+        assert.stub(dispatcher.dispatch).was_not.called()
+        tsnn_module.onreceive(conn, testchunk2)
+        assert.stub(dispatcher.dispatch).was.called_with(match._, "0123456789")
+        dispatcher.dispatch:revert()
+      end)
+    it("should be tested on random data", function ()
+        pending("add random data tests like of libdbus")
+      end)
+    
 end)
--- tango/server/nodemcu/net.lua
+++ tango/server/nodemcu/net.lua
@@ -1,6 +1,7 @@
 -- require "net" -- which is the NodeMCU core `net` module
 
 local print = print
+local tonumber = tonumber
 local dispatcher = require "tango.dispatcher"
 local default = require "tango.config".server_default
 
@@ -20,9 +21,37 @@ loop =
     -- TODO
   end
 
+local buf -- accumulating received bytes
+local msg_len=0 -- if 0 then we're in header phase, otherwise in msg phase
 onreceive =
   function(conn, chunk)
-    dispatcher:dispatch("0123456789")
+    if not buf then
+      buf = chunk
+    else
+      buf = buf .. chunk
+    end
+    
+    -- consume buffer line by line
+    while #buf > 0 do
+      if msg_len == 0 then -- in header
+        local e = buf:find("\n", 1, true)
+        if not e then return end -- incomplete, go wait for the rest of it
+        msg_len = tonumber( buf:sub(1,e-1) )
+        if not msg_len then
+          print("PANIC! broken message!")
+          buf = buf:sub(e+1)
+        end
+        buf = buf:sub(e+1)
+      else -- in message
+        if #buf < msg_len then return end -- incomplete
+        local msg = buf:sub(1, msg_len)
+        buf = buf:sub(msg_len+1)
+        msg_len = 0
+        dispatcher:dispatch(msg)
+        -- msg = nil
+      end
+      -- TODO: collect some garbage?
+    end
   end
   
 return {
