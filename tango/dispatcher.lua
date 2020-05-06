f661f0335a775e064f6bbedd0c33dddbc11d52b3
Ildar Mulyukov <ildar@altlinux.ru>
2020-05-06 17:44:22 +0600

    dispatcher: refactoring: extract `__mkref` method

--- tango/dispatcher.lua
+++ tango/dispatcher.lua
@@ -26,33 +26,33 @@ local new =
           local last_part
           local last_var
           for part in var_name:gmatch('[%w_]+') do
-             last_part = part
-             last_var = var
-             if type(var) == 'table' or type(var) == 'romtable' then
-                var = var[part]
+            last_part = part
+            last_var = var
+            if type(var) == 'table' or type(var) == 'romtable' then
+              var = var[part]
             else
-               return {false,error_msg(var_name,'no such variable')}
+              return {false,error_msg(var_name,'no such variable')}
             end  
           end        
           if type(var) == 'function' or type(var) == 'lightfunction' then
-             return {self.pcall(var,unpack(request,2))}
+            return {self.pcall(var,unpack(request,2))}
           else
-             local val = request[2]
-             if val then
-                if not self.write_access then
-                   return {false,error_msg(var_name,'no write_access')}
-                else
-                   return {self.pcall(
+            local val = request[2]
+            if val then
+              if not self.write_access then
+                return {false,error_msg(var_name,'no write_access')}
+              else
+                return {self.pcall(
                               function()
                                  last_var[last_part] = val
                               end)}
-                end
-             else
-                if not self.read_access then
-                   return {false,error_msg(var_name,'no read_access')}
-                else
-                   return {true,var}
-                end
+              end
+            else
+              if not self.read_access then
+                 return {false,error_msg(var_name,'no read_access')}
+              else
+                 return {true,var}
+              end
              end
           end        
           
@@ -62,18 +62,21 @@ local new =
     d.refs = {}
     d.functab.tango = d.functab.tango or {}
     
+    function d.functab.tango.__mkref(obj)
+      if type(obj) == 'table' or type(obj) == 'romtable' or type(obj) == 'userdata' then
+        local id = tostring(obj)
+        d.refs[id] = obj
+        return id
+      else
+        error('tango.ref proxy did not create table nor userdata')
+      end
+    end
+    
     d.functab.tango.ref_create = 
       function(create_method,...)
         local result = d:dispatch({create_method,...})
         if result[1] == true then
-          local obj = result[2]
-          if type(obj) == 'table' or type(obj) == 'romtable' or type(obj) == 'userdata' then
-            local id = tostring(obj)
-            d.refs[id] = obj
-            return id
-          else
-            error('tango.ref proxy did not create table nor userdata')
-          end
+          return d.functab.tango.__mkref(result[2])
         else
           error(result[2])
         end
